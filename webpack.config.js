d020501b78c2486476c4e78b7f06883ceeb6de71
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-15 21:27:23 +0530

    did the intial setup upto the webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        port: 3000,
+        hot: true,
+        open: true,
+        compress: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|png|svg)/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'ToDo List',
+            filename: 'index.html',
+            template: './src/template.html',
+            // favicon: "./src/assets/images/ship.svg",
+        }),
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
