f82da0f9fdaba2cc29bcb65191a05adb2eb5c927
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-02-21 16:11:18 +0530

    did the linting things

--- src/modules/html_elements/load_page.js
+++ src/modules/html_elements/load_page.js
@@ -3,26 +3,26 @@ import { makeElement } from "./html_create_function";
 const firstPage = (function initFirstPage() {
     
     function makeToDoListContainer() {
-        const body = document.querySelector('body');
-        const toDoListContainer = makeElement({id: 'mainContainer'});
+        const body = document.querySelector("body");
+        const toDoListContainer = makeElement({id: "mainContainer"});
         body.appendChild(toDoListContainer);
     }
 
     function makeHeader() {
-        const mainContainer = document.getElementById('mainContainer');
-        const header = makeElement({elementType: 'header'});
+        const mainContainer = document.getElementById("mainContainer");
+        const header = makeElement({elementType: "header"});
         mainContainer.appendChild(header);
     }
 
     function makeMain() {
-        const mainContainer = document.getElementById('mainContainer');
-        const main = makeElement({elementType: 'main'});
+        const mainContainer = document.getElementById("mainContainer");
+        const main = makeElement({elementType: "main"});
         mainContainer.appendChild(main);
     }
 
     function makeTitle() {
-        const header = document.querySelector('header');
-        const h1Title = makeElement({elementType: 'h1', textContent: 'To Do List', title: 'To Do List'});
+        const header = document.querySelector("header");
+        const h1Title = makeElement({elementType: "h1", textContent: "To Do List", title: "To Do List"});
         header.appendChild(h1Title);
     }
 
