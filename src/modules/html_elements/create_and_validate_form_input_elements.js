5b789d1ea424e9afe8b4733d298c796ce10bfae4
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-04 17:54:43 +0530

    created function makeFormDateInput

--- src/modules/html_elements/create_and_validate_form_input_elements.js
+++ src/modules/html_elements/create_and_validate_form_input_elements.js
@@ -1,17 +1,23 @@
 import { makeInput } from "./html_create_function";
 
-const createFormsElements = (function initFromElements() {
+const createFormsElements = (function initFormElements() {
 
-    function makeFromTitleInput() {
+    function makeFormTitleInput() {
         const form = document.querySelector("form");
         const inputTitle = makeInput({type: "name", name: "title", minLength: "3", maxLength: "50", placeholder: "Title", required: "true"});
         form.appendChild(inputTitle);
     }
 
-    function makeFromDescriptionInput() {
+    function makeFormDescriptionInput() {
         const form = document.querySelector("form");
         const inputDescription = makeInput({type: "text", name: "description", minLength: "20", maxLength: "80", placeholder: "Task Description", required: "true"});
         form.appendChild(inputDescription);
+    }
+
+    function makeFormDateInput() {
+        const form = document.querySelector("form");
+        const inputDate = makeInput({type: "date", name: "dueDate", required: "true"});
+        form.appendChild(inputDate);
     }
 
 })();
