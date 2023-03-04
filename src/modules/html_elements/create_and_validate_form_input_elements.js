e172a0158ddf1329a31974a217df3db14a103e3c
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-04 17:55:34 +0530

    in function makeformTitle change the type to text and min len to 5 and max len to 40

--- src/modules/html_elements/create_and_validate_form_input_elements.js
+++ src/modules/html_elements/create_and_validate_form_input_elements.js
@@ -4,7 +4,7 @@ const createFormsElements = (function initFormElements() {
 
     function makeFormTitleInput() {
         const form = document.querySelector("form");
-        const inputTitle = makeInput({type: "name", name: "title", minLength: "3", maxLength: "50", placeholder: "Title", required: "true"});
+        const inputTitle = makeInput({type: "text", name: "title", minLength: "5", maxLength: "40", placeholder: "Title", required: "true"});
         form.appendChild(inputTitle);
     }
 
