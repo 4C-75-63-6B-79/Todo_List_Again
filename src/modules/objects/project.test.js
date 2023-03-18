9234b840be3e9dad723fd777e78fa91b4e386588
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-18 18:10:18 +0530

    added test to test the add task function

--- src/modules/objects/project.test.js
+++ src/modules/objects/project.test.js
@@ -7,3 +7,9 @@ test("Project returns a object which has title and taskList as own property", ()
     expect(Object.prototype.hasOwnProperty.call(testProject, "title")).toBe(true);
     expect(Object.prototype.hasOwnProperty.call(testProject, "taskList")).toBe(true);
 });
+
+test("function add task add a task to the task list in project", () => {
+    const testProject = new Project("test project");
+    testProject.addTask({ title: "test task", description: "to test the add function" });
+    expect(testProject.taskList.length).toBe(1);
+});
