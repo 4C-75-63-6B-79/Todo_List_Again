9d24dfe66b20534a6d671cc28e94c44ac09022c8
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-18 18:10:59 +0530

    added the addtask function in project to pass the test

--- src/modules/objects/project.js
+++ src/modules/objects/project.js
@@ -1,5 +1,12 @@
+import Task from "./task";
+
 export default function Project({ title }) {
     this.title = title;
     this.taskList = [];
 }
+
+Project.prototype.addTask = function addTask({ title, description }) {
+    const task = new Task({ title, description });
+    this.taskList.push(task);
+};
 
