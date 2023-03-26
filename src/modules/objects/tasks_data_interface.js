04d929246590b77d9596a278d42cf6417ebbbb4c
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2023-03-26 19:27:32 +0530

    created a interface for porjects and tasks

---
+++ src/modules/objects/tasks_data_interface.js
@@ -0,0 +1,12 @@
+import Project from "./project";
+
+const interfaceProjectTask = (function initInterface() {
+
+    const allTaskList = new Project("All Task List");
+    const todaysTaskList = new Project("Today's Task List");
+    const nextSevenDayTaskList = new Project("Next Seven Day Task List");
+    const importantTaskList = new Project("next 7 day task list");
+
+    let currentTaskList;
+
+})();
